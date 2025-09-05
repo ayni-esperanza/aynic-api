02b89c7bfc318c0afc887e5d3515ba39328f35bb
Jhoneiro <jhoneiro12@hotmail.com>
2025-09-05 17:21:53 -0500

    agregando public al health

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Public } from '../auth/decorators/auth.decorators';
 
 @ApiTags('health')
 @Controller('health')
 export class HealthController {
+  @Public()
   @Get()
   @ApiOperation({ summary: 'Health check endpoint' })
   @ApiResponse({
