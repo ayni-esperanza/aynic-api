56b4e82e038666d7aecb97967d91563e30970606
Jhoneiro <jhoneiro12@hotmail.com>
2025-12-09 10:15:56 -0500

    configurando dokploy y prefijo para evitar acoplamiento

--- src/main.ts
+++ src/main.ts
@@ -52,7 +52,9 @@ function buildCorsOrigin() {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('v1');
+  app.setGlobalPrefix('v1', {
+    exclude: ['health'],
+  });
 
   const corsOrigin = buildCorsOrigin();
   const allowCredentials = process.env.CORS_CREDENTIALS === 'true';
